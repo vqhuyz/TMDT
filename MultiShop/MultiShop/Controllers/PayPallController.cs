41146552e79cb11c11bbeeb7414854827c3917a5
sinhvien <sinhvien>
2020-10-16 10:36:17 +0700

    1

---
+++ MultiShop/MultiShop/Controllers/PayPallController.cs
@@ -0,0 +1,255 @@
+﻿using log4net.Repository.Hierarchy;
+using Microsoft.Owin.BuilderProperties;
+using PayPal.Api;
+using System;
+using System.Collections.Generic;
+using MultiShop.Models;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Address = PayPal.Api.Address;
+
+namespace MultiShop.Controllers
+{
+    public class PayPallController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentWithPaypal()
+        {
+           
+            APIContext apiContext = Configuration.GetAPIContext();// Lấy apicontext ở class configuration
+            //
+            try
+            {
+                string payerId = Request.Params["PayerID"];
+
+                if (string.IsNullOrEmpty(payerId))
+                {
+                    //Khoảng code này sẽ đưuọc thực thi trước vì chưa có id payer (id người mua)
+                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority +"/Paypall/PaymentWithPayPal?";
+
+
+                    var guid = Convert.ToString((new Random()).Next(100000));
+
+
+                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
+
+             
+
+                    var links = createdPayment.links.GetEnumerator();
+
+                    string paypalRedirectUrl = null;
+
+                    while (links.MoveNext())
+                    {
+                        Links lnk = links.Current;
+
+                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
+                        {
+                            
+                            paypalRedirectUrl = lnk.href;
+                        }
+                    }
+
+                  
+                    Session.Add(guid, createdPayment.id);
+
+                    return Redirect(paypalRedirectUrl);
+                }
+                else
+                {
+                   
+
+                    var guid = Request.Params["guid"];
+
+                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
+
+                    if (executedPayment.state.ToLower() != "approved")
+                    {
+                        return View("FailureView");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+    
+                return View("FailureView");
+            }
+
+            return View("SuccessView");
+        }
+        private PayPal.Api.Payment payment;
+        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
+        {
+            var paymentExecution = new PaymentExecution() { payer_id = payerId };
+            this.payment = new Payment() { id = paymentId };
+            return this.payment.Execute(apiContext, paymentExecution);
+        }
+        private Payment CreatePayment(APIContext apiContext, string redirectUrl)
+        {
+
+            var itemList = new ItemList() { items = new List<Item>() };
+
+            itemList.items.Add(new Item()
+            {
+                name = "Item Name",
+                currency = "USD",
+                price = "5",
+                quantity = "1",
+                sku = "sku"
+            });
+
+            var payer = new Payer() { payment_method = "paypal" };
+
+            var redirUrls = new RedirectUrls()
+            {
+                cancel_url = redirectUrl,
+                return_url = redirectUrl
+            };
+
+            var details = new Details()
+            {
+                tax = "1",
+                shipping = "1",
+                subtotal = "5"
+            };
+
+            var amount = new Amount()
+            {
+                currency = "USD",
+                total = "7", // Total must be equal to sum of shipping, tax and subtotal.
+                details = details
+            };
+
+            var transactionList = new List<Transaction>();
+
+            transactionList.Add(new Transaction()
+            {
+                description = "Transaction description.",
+                invoice_number = "your invoice number",
+                amount = amount,
+                item_list = itemList
+            });
+
+            this.payment = new Payment()
+            {
+                intent = "sale",
+                payer = payer,
+                transactions = transactionList,
+                redirect_urls = redirUrls
+            };
+
+         
+            return this.payment.Create(apiContext);
+        }
+        public ActionResult PaymentWithCreditCard()
+        {
+            Item item = new Item();
+            item.name = "Demo Item";
+            item.currency = "USD";
+            item.price = "5";
+            item.quantity = "1";
+            item.sku = "sku";
+
+            List<Item> itms = new List<Item>();
+            itms.Add(item);
+            ItemList itemList = new ItemList();
+            itemList.items = itms;
+
+            Address billingAddress = new Address();
+            billingAddress.city = "NewYork";
+            billingAddress.country_code = "US";
+            billingAddress.line1 = "23rd street kew gardens";
+            billingAddress.postal_code = "43210";
+            billingAddress.state = "NY";
+
+
+            CreditCard crdtCard = new CreditCard();
+            crdtCard.billing_address = billingAddress;
+            crdtCard.cvv2 = "874";  //card cvv2 number
+            crdtCard.expire_month = 1; //card expire date
+            crdtCard.expire_year = 2020; //card expire year
+            crdtCard.first_name = "Aman";
+            crdtCard.last_name = "Thakur";
+            crdtCard.number = "1234567890123456"; //enter your credit card number here
+            crdtCard.type = "visa"; //credit card type here paypal allows 4 types
+
+            // Specify details of your payment amount.
+            Details details = new Details();
+            details.shipping = "1";
+            details.subtotal = "5";
+            details.tax = "1";
+
+            // Specify your total payment amount and assign the details object
+            Amount amnt = new Amount();
+            amnt.currency = "USD";
+            // Total = shipping tax + subtotal.
+            amnt.total = "7";
+            amnt.details = details;
+
+            // Now make a transaction object and assign the Amount object
+            Transaction tran = new Transaction();
+            tran.amount = amnt;
+            tran.description = "Description about the payment amount.";
+            tran.item_list = itemList;
+            tran.invoice_number = "your invoice number which you are generating";
+
+            // Now, we have to make a list of transaction and add the transactions object
+            // to this list. You can create one or more object as per your requirements
+
+            List<Transaction> transactions = new List<Transaction>();
+            transactions.Add(tran);
+
+            // Now we need to specify the FundingInstrument of the Payer
+            // for credit card payments, set the CreditCard which we made above
+
+            FundingInstrument fundInstrument = new FundingInstrument();
+            fundInstrument.credit_card = crdtCard;
+
+            // The Payment creation API requires a list of FundingIntrument
+
+            List<FundingInstrument> fundingInstrumentList = new List<FundingInstrument>();
+            fundingInstrumentList.Add(fundInstrument);
+
+            // Now create Payer object and assign the fundinginstrument list to the object
+            Payer payr = new Payer();
+            payr.funding_instruments = fundingInstrumentList;
+            payr.payment_method = "credit_card";
+
+            // finally create the payment object and assign the payer object & transaction list to it
+            Payment pymnt = new Payment();
+            pymnt.intent = "sale";
+            pymnt.payer = payr;
+            pymnt.transactions = transactions;
+
+            try
+            {
+                APIContext apiContext = Configuration.GetAPIContext();
+
+                
+
+                Payment createdPayment = pymnt.Create(apiContext);
+
+                //if the createdPayment.state is "approved" it means the payment was successful else not
+
+                if (createdPayment.state.ToLower() != "approved")
+                {
+                    return View("FailureView");
+                }
+            }
+            catch (PayPal.PayPalException ex)
+            {
+                //Logger.Log("Error: " + ex.Message);
+                return View("FailureView");
+            }
+
+            return View("SuccessView");
+        }
+
+    }
+}
---
+++ MultiShop/MultiShop/Models/Configuration.cs
@@ -0,0 +1,45 @@
+﻿using PayPal.Api; 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultiShop.Models
+{
+    public static class Configuration
+    {
+        public readonly static string ClientId;
+        public readonly static string ClientSecret;
+     // Biến lưu key và id trong web config
+        static Configuration()
+        {
+            var config = GetConfig();
+            ClientId = config["clientId"];
+            ClientSecret = config["clientSecret"];
+        }
+       
+        public static Dictionary<string, string> GetConfig()
+        {
+            // lấy thuộc tính trong web config
+            return PayPal.Api.ConfigManager.Instance.GetProperties();
+        }
+
+        private static string GetAccessToken()
+        {
+            // Lấy mã đăng nhập 
+            string accessToken = new OAuthTokenCredential(ClientId, ClientSecret, GetConfig()).GetAccessToken();
+            return accessToken;
+        }
+
+       
+        public static APIContext GetAPIContext()
+        {
+            // thông qua mã đăng nhập chúng ta lấy được apicontext
+            APIContext apiContext = new APIContext(GetAccessToken());
+            apiContext.Config = GetConfig();
+
+            return apiContext;
+        }
+
+    }
+}
